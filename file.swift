836e62a0400949b8b1e18555eca5ab0053517262
Heba Omar <97067717+hebaomar94@users.noreply.github.com>
2023-07-27 09:41:41 +0300

    Update file.swift

--- file.swift
+++ file.swift
@@ -1,1 +1,13 @@
+/*
+swift days 5 we will discuss the if condtion 
+1-declare the condation then we will put if condition */
 
+let age = 18 
+if age >= 18 {
+  print("go on")
+}
+let userName ="hi"
+if username == " "{
+  print("hello \(userName))
+       }
+  
