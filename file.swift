8ffeab25f1e00045a58f37bf1d1c04095619fcc5
Heba Omar <97067717+hebaomar94@users.noreply.github.com>
2023-07-27 10:13:37 +0300

    Update file.swift

--- file.swift
+++ file.swift
@@ -11,3 +11,20 @@ if userName == " "{
   print("hello \(userName)")
        }
   
+//How to check multiple conditions
+//we have && and (must two true)
+//or || pipe (must one true)
+//we can use enum
+/* Enums, short for enumerations, are a valuable data type in programming that allows you to define a set of related named values. 
+They provide a way to represent a 
+fixed number of possible states or options for a particular variable. In the example you provided, the enum */
+
+enum transport {
+  case airplane , bicycle , car
+}
+let trans = transport.car 
+if trans == .car {
+  print ("god one")
+} else {
+  print ("no way")
+}
